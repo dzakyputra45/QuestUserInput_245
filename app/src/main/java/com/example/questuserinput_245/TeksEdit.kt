c16932794c53f737fab9b58c26ad581548b456dd
dzakyputra45 <dzakypratama45@gmail.com>
2025-10-30 13:59:00 +0700

    Tambahkan horizontaldivider dan atur warnanya

--- app/src/main/java/com/example/questuserinput_245/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_245/TeksEdit.kt
@@ -94,6 +94,15 @@ fun FormatDataDiri(modifier: Modifier = Modifier) {
         ) {
             Text(stringResource(R.string.submit))
         }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium)
+            ),
+            thickness = dimensionResource(R.dimen.padding_tipis),
+            color = Color.DarkGray
+        )
     }
 
 
