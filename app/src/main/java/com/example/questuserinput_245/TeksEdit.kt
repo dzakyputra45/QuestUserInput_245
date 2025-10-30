0718be98d4279eba7bdd60bf38848c96d62d96ee
dzakyputra45 <dzakypratama45@gmail.com>
2025-10-30 13:44:55 +0700

    Tambahkan output states

---
+++ app/src/main/java/com/example/questuserinput_245/TeksEdit.kt
@@ -0,0 +1,28 @@
+package com.example.questuserinput_245
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormatDataDiri(modifier: Modifier = Modifier) {
+
+    // Input states
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textjk by remember { mutableStateOf("") }
+
+    // Output states
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+    }
+
