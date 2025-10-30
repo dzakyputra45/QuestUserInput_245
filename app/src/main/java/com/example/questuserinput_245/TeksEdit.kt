80c8d5f9ab5fd2637b72eab36612c8c1d28b38cf
dzakyputra45 <dzakypratama45@gmail.com>
2025-10-30 14:01:19 +0700

    Tambahkan elevated card dan ubah ukurannya

--- app/src/main/java/com/example/questuserinput_245/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_245/TeksEdit.kt
@@ -103,6 +103,14 @@ fun FormatDataDiri(modifier: Modifier = Modifier) {
             thickness = dimensionResource(R.dimen.padding_tipis),
             color = Color.DarkGray
         )
+
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(100.dp)
+                .width(300.dp)
+        ) {
     }
 
 
